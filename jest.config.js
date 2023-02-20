01010953e8988f17f993b9f6bc4ed38c5d4b94e1
Joe Burgess <josephburgess@gmail.com>
2023-02-20 16:08:34 +0200

    amending jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/__tests__'],
+  roots: ['<rootDir>/tests'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
