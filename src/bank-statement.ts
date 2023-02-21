906328b0cc6500682ea37c0da34d357ff2bc14dd
Joe Burgess <josephburgess@gmail.com>
2023-02-21 08:41:58 +0200

    refactoring bank statement class for readability

--- src/bank-statement.ts
+++ src/bank-statement.ts
@@ -8,26 +8,26 @@ export class BankStatement {
   }
 
   getStatement(): string {
-    let header = 'Date || Credit || Debit || Balance';
+    const header = this.getHeader();
+    const statementLines = this.getStatementLines();
+    return [header, ...statementLines.reverse()].join('\n');
+  }
+
+  private getHeader(): string {
+    return 'Date || Credit || Debit || Balance';
+  }
+
+  getStatementLines(): string[] {
     let runningBalance = 0;
-    const statementLines: string[] = [];
-    for (const transaction of this.account.transactions) {
-      const dateString = transaction.date.toISOString().substring(0, 10);
-      let credit = '';
-      let debit = '';
-      if (transaction.type === 'deposit') {
-        credit = transaction.amount.toFixed(2);
-        runningBalance += transaction.amount;
-      } else {
-        debit = transaction.amount.toFixed(2);
-        runningBalance -= transaction.amount;
-      }
-      const transactionString = `${dateString} || ${credit} || ${debit} || ${runningBalance.toFixed(
+    return this.account.transactions.map(({ date, amount, type }) => {
+      const dateString = date.toISOString().substring(0, 10);
+      const [credit, debit] =
+        type === 'deposit' ? [amount.toFixed(2), ''] : ['', amount.toFixed(2)];
+      runningBalance =
+        type === 'deposit' ? runningBalance + amount : runningBalance - amount;
+      return `${dateString} || ${credit} || ${debit} || ${runningBalance.toFixed(
         2
       )}`;
-      statementLines.push(transactionString);
-    }
-    const reversedStatementLines = [header, ...statementLines.reverse()];
-    return reversedStatementLines.join('\n');
+    });
   }
 }
